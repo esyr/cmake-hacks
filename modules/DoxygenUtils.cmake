f2e84118aeb23957bd1f910b2de4d9bfe6831d4d
eSyr <evgsyr@gmail.com>
2013-08-09 17:49:08 +0400

    DoxygenUtils.cmake: Multiple changes.  * Format parameters defaults moved to global defaults  * Default template path is now also global-wide cache value  * QUIET option added to generate_doxygen()  * Marker hash now includes some additional parameters, not only args

--- modules/DoxygenUtils.cmake
+++ modules/DoxygenUtils.cmake
@@ -1,29 +1,65 @@
 set(_doxygen_utils_listfile_dir "${CMAKE_CURRENT_LIST_DIR}" CACHE INTERNAL "Current list dir for doxygen utils module. Used for template discovery.")
+set(_doxygen_default_template_path "${_doxygen_utils_listfile_dir}/../templates/Doxyfile.template" CACHE INTERNAL "Path to default doxygen configuration template")
+
+# Some default documentation-related options
+set(CMAKE_DOC_MAN_SUBDIR "man"
+	CACHE STRING "Subdirectory for storing generated manpages")
+set(CMAKE_DOC_MAN_INSTALL_DIR "${CMAKE_INSTALL_MANDIR}"
+	CACHE PATH "Installation directory for manpages")
+set(CMAKE_DOC_MAN_APPEND_TARGET_NAME 0
+	CACHE BOOL "Append target name to the manpages documentation installation path")
+set(CMAKE_DOC_MAN_APPEND_SUBDIR 0
+	CACHE BOOL "Append manpages subdir set to the documentation installation path")
+
+set(CMAKE_DOC_HTML_SUBDIR "html"
+	CACHE STRING "Subdirectory for storing generated HTML help files")
+set(CMAKE_DOC_HTML_INSTALL_DIR "${CMAKE_INSTALL_DATAROOTDIR}/doc/"
+	CACHE PATH "Installation directory for HTML help files")
+set(CMAKE_DOC_HTML_APPEND_TARGET_NAME 1
+	CACHE BOOL "Append target name to the HTML documentation installation path")
+set(CMAKE_DOC_HTML_APPEND_SUBDIR 1
+	CACHE BOOL "Append HTML subdir to the documentation installation path")
+
+set(CMAKE_DOC_LATEX_SUBDIR "latex"
+	CACHE STRING "Subdirectory for storing generated LaTeX documentation files")
+set(CMAKE_DOC_LATEX_INSTALL_DIR "${CMAKE_INSTALL_DATAROOTDIR}/doc/"
+	CACHE PATH "Installation directory for LaTeX documentation")
+set(CMAKE_DOC_LATEX_APPEND_TARGET_NAME 1
+	CACHE BOOL "Append target name to the LaTeX documentation installation path")
+set(CMAKE_DOC_LATEX_APPEND_SUBDIR 1
+	CACHE BOOL "Append LaTeX subdir to the documentation installation path")
+
+set(CMAKE_DOC_RTF_SUBDIR "rtf"
+	CACHE STRING "Subdirectory for storing generated RTF documentation")
+set(CMAKE_DOC_RTF_INSTALL_DIR "${CMAKE_INSTALL_DATAROOTDIR}/doc/"
+	CACHE PATH "Installation directory for RTF documentation")
+set(CMAKE_DOC_RTF_APPEND_TARGET_NAME 1
+	CACHE BOOL "Append target name to the RTF documentation installation path")
+set(CMAKE_DOC_RTF_APPEND_SUBDIR 1
+	CACHE BOOL "Append RTF subdir to the documentation installation path")
+
+set(CMAKE_DOC_DOXYGEN_QUIET 1
+	CACHE BOOL "Run doxygen quietly")
+
+foreach (_format MAN HTML LATEX RTF)
+	foreach (_opt SUBDIR INSTALL_DIR APPEND_TARGET_NAME APPEND_SUBDIR)
+		mark_as_advanced(CMAKE_DOC_${_format}_${_opt})
+	endforeach ()
+endforeach ()
 
 function (add_doxygen _TARGET_NAME)
 	include(GNUInstallDirs)
 
 	set(_formats MAN HTML LATEX RTF)
 
-	set(GENERATE_DOXYGEN_MAN_SUBDIR "man")
-	set(GENERATE_DOXYGEN_MAN_INSTALL_DIR "${CMAKE_INSTALL_MANDIR}")
-	set(GENERATE_DOXYGEN_MAN_APPEND_TARGET_NAME 0)
-	set(GENERATE_DOXYGEN_MAN_APPEND_SUBDIR 0)
-
-	set(GENERATE_DOXYGEN_HTML_SUBDIR "html")
-	set(GENERATE_DOXYGEN_HTML_INSTALL_DIR "${CMAKE_INSTALL_DATAROOTDIR}/doc/")
-	set(GENERATE_DOXYGEN_HTML_APPEND_TARGET_NAME 1)
-	set(GENERATE_DOXYGEN_HTML_APPEND_SUBDIR 1)
-
-	set(GENERATE_DOXYGEN_LATEX_SUBDIR "latex")
-	set(GENERATE_DOXYGEN_LATEX_INSTALL_DIR "${CMAKE_INSTALL_DATAROOTDIR}/doc/")
-	set(GENERATE_DOXYGEN_LATEX_APPEND_TARGET_NAME 1)
-	set(GENERATE_DOXYGEN_LATEX_APPEND_SUBDIR 1)
-
-	set(GENERATE_DOXYGEN_RTF_SUBDIR "rtf")
-	set(GENERATE_DOXYGEN_RTF_INSTALL_DIR "${CMAKE_INSTALL_DATAROOTDIR}/doc/")
-	set(GENERATE_DOXYGEN_RTF_APPEND_TARGET_NAME 1)
-	set(GENERATE_DOXYGEN_RTF_APPEND_SUBDIR 1)
+	# Setting format defaults
+	set(_param_defaults)
+	foreach (_format ${_formats})
+		foreach (_opt SUBDIR INSTALL_DIR APPEND_TARGET_NAME APPEND_SUBDIR)
+			set(GENERATE_DOXYGEN_${_format}_${_opt} "${CMAKE_DOC_${_format}_${_opt}}")
+			set(_param_defaults "${_param_defaults}|${CMAKE_DOC_${_format}_${_opt}}")
+		endforeach ()
+	endforeach ()
 
 	foreach (_format ${_formats})
 		list(APPEND _install_opts "${_formats}_APPEND_TARGET_NAME" "${_formats}_APPEND_SUBDIR")
@@ -31,7 +67,7 @@ function (add_doxygen _TARGET_NAME)
 	endforeach ()
 
 	set(_Options ${_formats} ${_install_opts})
-	set(_OneValueArgs MAN_SECTION TEMPLATE PROJECT_NAME FILES_TARGET_NAME ${_install_ova})
+	set(_OneValueArgs MAN_SECTION TEMPLATE PROJECT_NAME FILES_TARGET_NAME ${_install_ova} QUIET)
 	set(_MultiValueArgs TARGETS FILES)
 	cmake_parse_arguments(GENERATE_DOXYGEN
 		"${_Options}"
@@ -44,19 +80,23 @@ function (add_doxygen _TARGET_NAME)
 	if (GENERATE_DOXYGEN_TEMPLATE)
 		set(_template "${GENERATE_DOXYGEN_TEMPLATE}")
 	else ()
-		set(_template "${_doxygen_utils_listfile_dir}/../templates/Doxyfile.template")
+		set(_template "${_doxygen_default_template_path}")
+	endif ()
+
+	if (NOT DEFINED GENERATE_DOXYGEN_QUIET)
+		set(GENERATE_DOXYGEN_QUIET "${CMAKE_DOC_DOXYGEN_QUIET}")
 	endif ()
 
 	# Otherwise changes in configuration but not in sources would not be noticed
 	file(SHA256 "${_template}" _template_hash)
-	string(SHA256 _marker_hash "${_TARGET_NAME}|${ARGN}|${_template_hash}")
+	string(SHA256 _marker_hash "${_TARGET_NAME}|${_param_defaults}|${ARGN}|${_template_hash}")
 
 	# Making format vars in doxygen format
-	foreach (_format ${_formats})
-		if (GENERATE_DOXYGEN_${_format})
-			set(GENERATE_DOXYGEN_${_format} "YES")
+	foreach (_opt ${_formats} QUIET)
+		if (GENERATE_DOXYGEN_${_opt})
+			set(GENERATE_DOXYGEN_${_opt} "YES")
 		else ()
-			set(GENERATE_DOXYGEN_${_format} "NO")
+			set(GENERATE_DOXYGEN_${_opt} "NO")
 		endif ()
 	endforeach ()
 
